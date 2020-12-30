9584bc3f3b555f08a505f8542c8cbb204e1fc86c
Salvador Cruz Durán <chavatores@hotmail.com>
2020-12-30 00:44:33 -0600

    agregado de archivos java

---
+++ VentanaIDE/src/main/java/com/unid/ventanaide/VentanaSourse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unid.ventanaide;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Salvador Cruz
+ */
+public class VentanaSourse extends JFrame{
+    
+public VentanaSourse(){
+this.setSize(400, 400);
+}
+
+public static void main(String[] args){
+Ventana win = new Ventana();
+win.setVisible(true);
+}
+
+}
+
